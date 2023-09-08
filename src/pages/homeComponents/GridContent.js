23fd6215996822d07e87ad85093af8b8cf0b7873
Evgeny Gerasymov <e.gerasymov@gmail.com>
2023-09-08 19:42:29 +0300

    added pagination

--- src/pages/homeComponents/GridContent.js
+++ src/pages/homeComponents/GridContent.js
@@ -4,13 +4,13 @@ import FiltersContext from "./FiltersContext";
 import { AppContext } from "../../AppContext";
 
 function GridContent() {
-  const [filteredData, setFilteredData] = useState();
+  const [filteredData, setFilteredData] = useState([]);
   const { queryParams } = useContext(FiltersContext);
   const { data } = useContext(AppContext);
-
+  const [page, setPage] = useState(0);
+  const [pageItems, setPageItems] = useState([]);
   useEffect(() => {
-    setFilteredData(data);
-
+    data && setFilteredData(data);
     data &&
       setFilteredData(
         data.filter((el) => {
@@ -20,15 +20,37 @@ function GridContent() {
             : el.name.common.toLowerCase().includes(queryParams.get("q"));
         })
       );
+    setPage(0);
+    setPageItems(filteredData.slice(0, 20));
   }, [queryParams, data]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setPage((prev) => prev + 1);
+    }
+  };
+  useEffect(() => {
+    page !== 0 && filteredData
+      ? setPageItems((prev) => [
+          ...prev,
+          ...filteredData.slice(page * 20, page * 20 + 20),
+        ])
+      : setPageItems(filteredData.slice(0, 20));
+  }, [page, filteredData]);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <main
       className={`grid place-content-center gap-10 mt-10 mx-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ${
-        filteredData?.length === 0 ? "h-screen" : ""
+        pageItems?.length === 0 ? "h-screen" : ""
       }`}
     >
-      {filteredData?.map((el, key) => {
+      {pageItems?.map((el, key) => {
         return (
           <React.Fragment key={key}>
             <Card data={el} />
